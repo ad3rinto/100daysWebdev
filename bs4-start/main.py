5522b77afcaebb0c5e1bab7e7e52292bee4f01cf
ad3rinto <aad3rinto@gmail.com>
2024-05-07 20:11:07 +0100

    give up

--- bs4-start/main.py
+++ bs4-start/main.py
@@ -1,25 +1,53 @@
+import requests
 from bs4 import BeautifulSoup
-# import lxml
 
-with open("./website.html") as data:
-    content = data.read()
-    # print(content)
+# URL
+URL = "https://news.ycombinator.com/news"
+
+# Get the webpage
+response = requests.get(URL)
+yc_webpage = response.text
+
+# Parse the HTML
+soup = BeautifulSoup(yc_webpage, "html.parser")
+
+# Find the titles and scores
+list_of_titles = soup.find_all('tr', class_="athing")
+score_of_titles = soup.find_all('span', class_="score")
+
+# Print the titles, links, and scores
+for title, score in zip(list_of_titles, score_of_titles):
+    title_text = title.find('a', class_="storylink")
+    link = "https://news.ycombinator.com/" + title.find('a', class_="storylink").get('href')
+    print(f"Title: {title_text}")
+    print(f"Link: {link}")
+    print(f"Score: {score.text}")
+    print("-" * 20)
 
 
-soup = BeautifulSoup(content, "html.parser")
-# print(soup.title)
-# print(soup.title.name)
-# print(soup.title.text)
-# print(soup.prettify())
 
-all_a_tags = soup.findAll(name="a")
-# print(all_a_tags)
-# for anc in all_a_tags:
-#     print(anc.get("href"))
-#     print(anc.text)
-#     print(anc.getText())
-heading = soup.find(id="name").get_text()
-print(heading)
-
-heading2 = soup.find(name='h3', class_="heading").get_text()
-print(heading2)
+# from bs4 import BeautifulSoup
+# # import lxml
+#
+# with open("./website.html") as data:
+#     content = data.read()
+#     # print(content)
+#
+#
+# soup = BeautifulSoup(content, "html.parser")
+# # print(soup.title)
+# # print(soup.title.name)
+# # print(soup.title.text)
+# # print(soup.prettify())
+#
+# all_a_tags = soup.findAll(name="a")
+# # print(all_a_tags)
+# # for anc in all_a_tags:
+# #     print(anc.get("href"))
+# #     print(anc.text)
+# #     print(anc.getText())
+# heading = soup.find(id="name").get_text()
+# print(heading)
+#
+# heading2 = soup.find(name='h3', class_="heading").get_text()
+# print(heading2)
--- bs4-start/main2.py
+++ bs4-start/main2.py
@@ -1,18 +1,45 @@
 import requests
 
 from bs4 import BeautifulSoup
+
 # import lxml
 
-URL = "https://news.ycombinator.com/"
+URL = "https://news.ycombinator.com/news"
 response = (requests.get(URL))
 yc_webpage = response.text
 
 soup = BeautifulSoup(yc_webpage, "html.parser")
 # print(soup.prettify())
 
-list_of_titles = soup.findAll(class_="titleline")
-for item in list_of_titles:
-    print(item)
+list_of_titles = soup.find_all('tr', class_="athing")
+score_of_titles = soup.find_all('span', class_="score")
+
+for title, score in zip(list_of_titles, score_of_titles):
+    print(f"{title.text}\nlink is 'https://news.ycombinator.com'{title.find('a', class_='athing')}\nScore {score.text}")
+
+# # Print the scores
+# for title, score in zip(list_of_titles, score_of_titles):
+#     print(title.find('a', class_="storylink").get("href"))
+
+# list_of_titles = soup.findAll(class_="titleline")
+# score_of_titles = soup.findAll(class_="score")
+# for item in list_of_titles:
+#     title = (item.getText())
+#     link = item.find('a').get('href')
+#     for anotherItem in score_of_titles:
+#         score = anotherItem.getText()
+#     print(score)
+
+    # score = item[score_of_titles]
+    # title = item[list_of_titles].getText()
+    # link = item[list_of_titles].find("a").get("href")
+    #
+    # print(f'This article with title {title} has {score}, find link to article here - {link}')
+
+# score_of_titles = soup.findAll(class_="score")
+# for item in score_of_titles:
+#     score = item.text
+#     print(score)
 
 
 # data = urllib.request.urlopen(URL)
@@ -27,5 +54,3 @@ for item in list_of_titles:
 # for item in all_a_tags:
 #     # print(item.get_text())
 #     print(item.select(".titleline a"))
-
-
