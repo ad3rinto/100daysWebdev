22a95f95d90f064e605d21596525f04c87cfc483
ad3rinto <aad3rinto@gmail.com>
2024-05-14 16:40:05 +0100

    find and create list of titles

--- 100 movies to watch start/main.py
+++ 100 movies to watch start/main.py
@@ -1,8 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+URL = "https://web.archive.org/web/20200611015421/https://www.empireonline.com/movies/features/best-tv-shows-ever-2/"
 
 # Write your code below this line 👇
 
+web_data = requests.get(URL)
+content = web_data.text
 
+soup = BeautifulSoup(content, "html.parser")
+
+title = soup.find_all("h3")
+
+list_of_titles = [name.text for name in title]
+print(list_of_titles)
